81a2df3cce4114d1e2568b88048aa6517224ec3c
web-thiru <murthithiru299@gmail.com>
2024-05-01 20:39:49 +0530

    Add project files.

---
+++ WebApplication1/Default.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="Home Page" Language="VB" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.vb" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    
+
+    <div class="row">
+        <div class="col-md-4">
+            <asp:GridView ID="GridView1" runat="server" CssClass="gridview">
+            </asp:GridView>
+            <br />
+        </div>
+    </div>
+
+</asp:Content>
---
+++ WebApplication1/Default.aspx.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Data.SqlClient
+
+Public Class _Default
+    Inherits Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        ' Replace with your connection string information
+        Dim connectionString As String = "Data Source=LAPTOP-EJRMOJDJ\MSSQLSERVER01;Initial Catalog=Data1;Integrated Security=True"
+        Dim connection As SqlConnection = New SqlConnection(connectionString)
+        Dim queryString As String = "SELECT * FROM users"
+        Dim dataAdapter As New SqlDataAdapter(queryString, connection)
+        Dim dataTable As New DataTable()
+        dataAdapter.Fill(dataTable)
+
+        ' Bind the DataTable to the GridView
+        GridView1.DataSource = dataTable
+        GridView1.DataBind()
+
+
+    End Sub
+
+
+End Class
